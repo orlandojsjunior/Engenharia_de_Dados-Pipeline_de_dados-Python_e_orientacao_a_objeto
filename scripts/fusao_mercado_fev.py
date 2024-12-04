bddf2b6bee2c4f1c92a3148580e0e8d175c09eb5
Orlando Junior <orlandojsjunior@hotmail.com>
2024-12-04 16:30:30 -0300

    Leitura de dados - script para fusão de dados e documentação no notebook de exploração

---
+++ scripts/fusao_mercado_fev.py
@@ -0,0 +1,45 @@
+import json
+import csv
+
+
+
+def leitura_jason(path_json):
+    dados_json = []
+    with open(path_json, 'r') as file:
+        dados_json = json.load(file)
+    return dados_json
+
+
+def leitura_csv(path_csv):
+    dados_csv = []
+    with open(path_csv, 'r') as file:
+        spamreader = csv.DictReader(file, delimiter=',')
+        for row in spamreader:
+            dados_csv.append(row)
+    return dados_csv
+
+
+def leitura_dados(path, tipo_arquivo):
+    dados = []
+
+    if tipo_arquivo == 'json':
+        dados = leitura_jason(path)
+
+    elif tipo_arquivo == 'csv':
+        dados = leitura_csv(path)
+    
+    return dados
+
+path_json = 'data_raw/dados_empresaA.json'
+path_csv = 'data_raw/dados_empresaB.csv'
+
+
+dados_json = leitura_dados(path_json, 'json')
+print(dados_json[0])
+
+dados_csv = leitura_dados(path_csv, 'csv')
+print(dados_csv[0])
+
+
+
+
