d8a953876eb4a8ee7cfe44048f91cc9ee53febe6
Orlando Junior <orlandojsjunior@hotmail.com>
2024-12-05 22:25:51 -0300

    Combinando os dados

--- scripts/fusao_mercado_fev.py
+++ scripts/fusao_mercado_fev.py
@@ -43,6 +43,17 @@ def rename_columns(dados, key_mapping):
     return new_dados_csv
 
 
+def size_data(dados):
+  return len(dados)
+
+
+def join(dadosA, dadosB):
+    combined_list = []
+    combined_list.extend(dadosA)
+    combined_list.extend(dadosB)
+    return combined_list
+
+
 # Definindo os paths dos arquivos
 path_json = 'data_raw/dados_empresaA.json'
 path_csv = 'data_raw/dados_empresaB.csv'
@@ -51,12 +62,21 @@ path_csv = 'data_raw/dados_empresaB.csv'
 # Iniciando a leitura
 dados_json = leitura_dados(path_json, 'json')
 nome_colunas_json = get_columns(dados_json)
-print(f"Nome colunas dados JSON: {nome_colunas_json}")
+tamanho_dados_json = size_data(dados_json)
+
+print(f"Nome das colunas dados JSON: {nome_colunas_json}")
+print(f"Tamanho dos dados JSON: {tamanho_dados_json}")
+
 
 dados_csv = leitura_dados(path_csv, 'csv')
 nome_colunas_csv = get_columns(dados_csv)
-print(f"Nome colunas dados CSV: {nome_colunas_csv}")
+tamanho_dados_csv = size_data(dados_csv)
 
+print(f"Nome das colunas dados CSV: {nome_colunas_csv}")
+print(f"Tamanho dos dados CSV: {tamanho_dados_csv}")
+
+
+# Transformação dos dados
 
 key_mapping = {'Nome do Item': 'Nome do Produto',
                             'Classificação do Produto': 'Categoria do Produto',
@@ -66,11 +86,16 @@ key_mapping = {'Nome do Item': 'Nome do Produto',
                             'Data da Venda': 'Data da Venda'}
 key_mapping
 
-
-# Transformação dos dados
-
 dados_csv = rename_columns(dados_csv, key_mapping)
-
 nome_colunas_csv = get_columns(dados_csv)
-
 print(f"Nome colunas Renomadas: {nome_colunas_csv}")
+
+
+# Fusão dos dados
+
+dados_fusao = join(dados_json, dados_csv)
+nomes_colunas_fusao = get_columns(dados_fusao)
+tamanho_dados_fusao = size_data(dados_fusao)
+
+print(f"Nome das Colunas Fusão: {nomes_colunas_fusao}")
+print(f"Tamanho dos dados após a Fusão: {tamanho_dados_fusao}")
